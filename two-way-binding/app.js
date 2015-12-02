333ebd695fa0edf8b55749d6b521d04f6e62c261
Julian Nicholls <juliannicholls29@gmail.com>
2015-12-02 11:38:40 +0000

    Add AJAX methods to retrieve and update rules.

--- two-way-binding/app.js
+++ two-way-binding/app.js
@@ -2,20 +2,21 @@
 var myApp = angular.module('myApp', []);
 
 // CONTROLLERS
-myApp.controller('mainController', ['$scope', function($scope) {
+myApp.controller('mainController', ['$scope', '$http', function($scope, $http) {
 
-    $scope.$watch('handle', function (new_val, old_val) {
-        console.info('Changed - Old: ' + old_val + ', New: ' + new_val);
-    });
+    // Watch the handle and report changes
+    // $scope.$watch('handle', function (new_val, old_val) {
+    //     console.debug('Changed - Old: ' + old_val + ', New: ' + new_val);
+    // });
 
-    // // This will go behind Angular's back, hence the $timeout service
+    // This will go behind Angular's back, hence the $timeout service
 
     // setTimeout(function () {
     //     console.debug('Changed!')
     //     $scope.handle = 'Nothing';
     // }, 2000)
 
-    // // This is another way to apply the change
+    // This is another way to apply the change
 
     // setTimeout(function () {
     //     $scope.$apply(function() {
@@ -26,15 +27,62 @@ myApp.controller('mainController', ['$scope', function($scope) {
 
     $scope.characters = 5;
 
-    $scope.rules = [
-        { rulename: 'must be 5 characters' },
-        { rulename: 'must not be used elsewhere' },
-        { rulename: 'must be cool' }
-    ];
+    // Static Rules
+    // $scope.rules = [
+    //     { rulename: 'must be 5 characters' },
+    //     { rulename: 'must not be used elsewhere' },
+    //     { rulename: 'must be cool' }
+    // ];
+
+    // Asynchronously collected rules
+    $http({
+        method: 'GET',
+        url: 'http://node.local:3100/api'
+    }).then(function successful(result) {
+            console.debug(result)
+            $scope.rules = result.data;
+            console.debug($scope.rules)
+        }, function failure(data, status) {
+            console.warn('GET Failed: ');
+            console.warn(data);
+        });
 
     $scope.clicked = function () {
         alert("Clicked!");
-    };
+    }
 
     $scope.name = "Julian Nicholls";
+
+    $scope.newRule = '';
+
+    // POST a new rule
+    $scope.addRule = function() {
+        $http({
+            method: 'POST',
+            url: 'http://node.local:3100/api',
+            data: { newRule: $scope.newRule }
+        }).then(function success(result) {
+            console.debug('POST Success');
+            $scope.rules = result.data;
+            $scope.newRule = '';
+        }, function error(data, status) {
+            console.warn("POST Failed:");
+            console.warn(data);
+        });
+    }
+
+    $scope.resetRules = function() {
+        $http({
+            method: 'POST',
+            url: 'http://node.local:3100/api',
+            data: { reset: 1 }
+        }).then(function success(result) {
+            console.debug('POST Success');
+            $scope.rules = result.data;
+            $scope.newRule = '';
+        }, function error(data, status) {
+            console.warn("POST Failed:");
+            console.warn(data);
+        });
+    }
 }]);
