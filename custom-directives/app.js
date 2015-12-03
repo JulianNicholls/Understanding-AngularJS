e9898d3dbae353c67a37f4015f0a075d39b149e1
Julian Nicholls <juliannicholls29@gmail.com>
2015-12-03 11:15:24 +0000

    Example with function

--- custom-directives/app.js
+++ custom-directives/app.js
@@ -18,19 +18,34 @@ myApp.config(function($routeProvider) {
 myApp.controller('mainController', ['$scope', function($scope) {
     $scope.person = {
         name: 'Jane Doe',
-        address: '555 5th Avenue, New York, NY 10023'
+        address: '555 5th Avenue',
+        city: 'New York',
+        state: 'NY',
+        zip: '10023'
     };
 
     $scope.people = [
         {
             name: 'Julian Nicholls',
-            address: 'Bournemouth, Dorset, UK'
+            address: '2132 Christchurch Road',
+            city: 'Bournemouth',
+            state: 'UK',
+            zip: 'BN12 3XX'
         },
         {
             name: 'John Doe',
-            address: '123 Fake St, Springfield, OR 74021'
+            address: '123 Fake St',
+            city: 'Springfield',
+            state: 'OR',
+            zip: '74021'
         },
     ];
+
+    $scope.formattedAddress = function(person) {
+        return person.address + ', ' + person.city + ', ' +
+            person.state + ' ' + person.zip;
+    };
+
 }]);
 
 myApp.controller('secondController', ['$scope', '$log', function($scope, $log) {
@@ -44,9 +59,10 @@ myApp.directive('searchResult', function () {
         templateUrl: 'directives/search-result.html',
         replace: true,  // Replace the directive with contents, default is to wrap
         scope: {        // Turn off access to controller scope
-            personName: "@",        // Text, needs {{ }}
+            personName: "@",                // Text, needs {{ }}
             personAddress: "@",
-            personObject: "=",      // Two-way binding object, no {{ }}
+            personObject: "=",              // Two-way binding object, no {{ }}
+            formattedAddressFunction: "&"   // Function, no {{ }}
         }
     }
 })
