41683670575e3aa6ab4f3c28f36df3be3b2d9696
Julian Nicholls <juliannicholls29@gmail.com>
2015-12-03 10:38:19 +0000

    Directive example with text scope.

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 My contributions from the Understanding AngularJS course at https://www.udemy.com/learn-angularjs/learn
 
-**NB**: If you use my files unchanged
+**NB**: If you use my files unchanged in the two-way-binding
 you will need to make some configuration changes. I have set up a number
 of aliases in my `/etc/hosts` file, including node.local, i.e.
 
@@ -17,5 +17,24 @@ of aliases in my `/etc/hosts` file, including node.local, i.e.
 Otherwise, you can replace the `'node.local'` in the AJAX calls
 with either `'localhost'` or `'127.0.0.1'`
 
-Feel free to contact me if you have any questions.
+To use the two-way-binding directory you need to install
+[Node](https://nodejs.org/). Then
 
+```bash
+cd two-way-binding
+npm install
+node index.js
+```
+
+I also suggest installing nodemon and using that to run your Node projects.
+That can be done with
+
+```bash
+npm install nodemon -g
+```
+
+You may need to use `sudo`.
+
+Then, point your browser at `http://node.local:3100`.
+
+Feel free to contact me if you have any questions.
---
+++ custom-directives/app.js
@@ -0,0 +1,46 @@
+// MODULE
+var myApp = angular.module('myApp', ['ngRoute']);
+
+myApp.config(function($routeProvider) {
+    $routeProvider.when('/', {
+        templateUrl: 'pages/main.html',
+        controller: 'mainController'
+    }).when('/second', {
+        templateUrl: 'pages/second.html',
+        controller: 'secondController'
+    }).when('/second/:num', {
+        templateUrl: 'pages/second.html',
+        controller: 'secondController'
+    });
+});
+
+// CONTROLLERS
+myApp.controller('mainController', ['$scope', function($scope) {
+    $scope.people = [
+        {
+            name: 'Julian Nicholls',
+            address: 'Bournemouth, Dorset, UK'
+        },
+        {
+            name: 'John Doe',
+            address: '123 Fake St, Springfield, OR 74021'
+        },
+    ]
+}]);
+
+myApp.controller('secondController', ['$scope', '$log', function($scope, $log) {
+
+}]);
+
+myApp.directive('searchResult', function () {
+    return {
+        restrict: 'AECM',     // Attribute and Element as default, Class and comMent turned on
+//      template: '<a><p> etc'
+        templateUrl: 'directives/search-result.html',
+        replace: true,  // Replace the directive with contents, default is to wrap
+        scope: {        // Turn off access to controller scope
+            personName: "@",     // Text
+            personAddress: "@"
+        }
+    }
+})
